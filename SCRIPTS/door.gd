1248cd001d864936cb920d9cf46cd9dc39a54d52
Stein <86646466+IamSTEINI@users.noreply.github.com>
2025-10-05 01:08:56 +0200

    Add door close sound effect and playback logic
    Added DOOR_CLOSE.mp3 sound asset and integrated an AudioStreamPlayer2D node into the Door scene. Updated door.gd script to play the sound when the door opens or closes.

--- SCRIPTS/door.gd
+++ SCRIPTS/door.gd
@@ -10,9 +10,11 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 	if npcs == 1:
 		if $AnimatedSprite2D.animation != "DOOR OPEN":
 			$AnimatedSprite2D.play("DOOR OPEN")
+			$DOOR_SOUND.play()
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
 	npcs = max(0, npcs - 1)
 	if npcs == 0:
 		if $AnimatedSprite2D.animation != "DOOR CLOSE":
 			$AnimatedSprite2D.play("DOOR CLOSE")
+			$DOOR_SOUND.play()
