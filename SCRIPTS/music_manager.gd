3f0f45754aa2d23bc2e01766d534ea6e898c78f6
Stein <86646466+IamSTEINI@users.noreply.github.com>
2025-10-21 22:38:50 +0200

    Add mashine_saver scene and enhance music manager UI
    Introduces the new MashineSaver scene and script, updates the music manager to support song skipping, automatic song cycling, and a UI overlay displaying the current song. Adds the 'A busy day' track, updates icons, and improves the shop tab tooltip and description.

--- SCRIPTS/music_manager.gd
+++ SCRIPTS/music_manager.gd
@@ -4,10 +4,11 @@ extends Node
 
 @onready var music: AudioStreamPlayer2D = $MUSIC
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
-
+var menutween: Tween
 var currently_playing: String = "Nothing is playing"
 
 func _ready() -> void:
+	music.finished.connect(_on_music_finished)
 	call_sound("Main Menu")
 
 func call_sound(song: String) -> void:
@@ -17,6 +18,8 @@ func call_sound(song: String) -> void:
 	music.stream = sounds[song]
 	currently_playing = song
 	music.play()
+	$CanvasLayer/TextureRect/Song.text = song
+	move_up()
 	
 func call_sound_with_fade(song: String) -> void:
 	if currently_playing == song:
@@ -25,3 +28,26 @@ func call_sound_with_fade(song: String) -> void:
 	await get_tree().create_timer(0.5).timeout
 	call_sound(song)
 	animation_player.play_backwards("fade")
+
+func _on_music_finished():
+	var keys = sounds.keys()
+	var current = keys.find(currently_playing)
+	if current != -1:
+		var next = (current + 1) % keys.size()
+		call_sound_with_fade(keys[next])
+
+func move_up():
+	if menutween:
+		menutween.kill()
+	menutween = create_tween()
+	menutween.set_ease(Tween.EASE_OUT)
+	menutween.set_trans(Tween.TRANS_BACK)
+	var orig_pos = 999
+	menutween.tween_property($CanvasLayer/TextureRect, "position:y", orig_pos + 100, 0.2)
+	menutween.tween_interval(0.2)
+	menutween.tween_property($CanvasLayer/TextureRect, "position:y", orig_pos, 0.2)
+
+
+func _on_skipping_pressed() -> void:
+	music.stop()
+	_on_music_finished()
