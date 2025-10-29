172235e93d30b2b228196ade7d952422ec9df74b
Stein <86646466+IamSTEINI@users.noreply.github.com>
2025-10-29 22:08:52 +0100

    Add virtual mouse and arcade joystick assets
    Introduces a virtual mouse system with new input actions and a script for simulating mouse movement and clicks via keyboard or gamepad. Adds new arcade joystick and cursor textures, updates the dev menu to visualize joystick input, and modifies arcade machine animations. Also updates the main menu and dev scenes for improved UI and input support, and disables looping for the CatSoundTrack.

--- SCRIPTS/dev_menu.gd
+++ SCRIPTS/dev_menu.gd
@@ -9,8 +9,19 @@ func _ready() -> void:
 func _process(_delta: float) -> void:
 	if Input.is_action_just_released("devmode"):
 		$CanvasLayer.visible = !$CanvasLayer.visible
-
-
+	
+	# Testing with IJKL and U as left and O as right click
+	if Input.is_action_pressed("vm_up"):
+		$CanvasLayer/JoyStick.play("UP")
+	elif Input.is_action_pressed("vm_down"):
+		$CanvasLayer/JoyStick.play("DOWN")
+	elif Input.is_action_pressed("vm_left"):
+		$CanvasLayer/JoyStick.play("LEFT")
+	elif Input.is_action_pressed("vm_right"):
+		$CanvasLayer/JoyStick.play("RIGHT")
+	else:
+		$CanvasLayer/JoyStick.play("IDLE")
+	
 func _on_time_skip_button_down() -> void:
 	Engine.time_scale = 20
 
